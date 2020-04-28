dac1b0bd3dd72a7f88233b907d35fa14ca6f4cf9
David Tchoulakian <d.tchoulakian@salt-and-pepper.eu>
2020-04-28 12:04:39 +0200

    v0.1 direct 1 to 1 control

---
+++ Unity Playground/Assets/Telekinesis/Scripts/ResetControlledObjectPosition.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Telekinesis
+{
+    public class ResetControlledObjectPosition : MonoBehaviour
+    {
+        public Transform ResetPoint;
+
+        void Start()
+        {
+            Reset();
+        }
+
+        private void Reset()
+        {
+            transform.position = ResetPoint.position;
+            transform.rotation = ResetPoint.rotation;
+        }
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Reset();
+            }
+        }
+    }
+}
---
+++ Unity Playground/Assets/Telekinesis/Scripts/v0.1/TelekinesisControlv01.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Telekinesis
+{
+    public class TelekinesisControlv01 : MonoBehaviour
+    {
+        public Transform Controller;
+
+        private Vector3 DistanceToController;
+        
+        void Start()
+        {
+            DistanceToController = Controller.position - transform.position;
+            SetPosition();
+        }
+
+        private void SetPosition()
+        {
+            transform.position = Controller.position + DistanceToController;
+            transform.rotation = Controller.rotation;
+        }
+
+        void Update()
+        {
+            SetPosition();
+        }
+    }
+}
