26d1e94dac00d656b315dc66dac8fd4816421c7c
Manuel-Ko <manuel.kosta@outlook.com>
2020-05-07 22:37:32 +0200

    add TestScripts for input actions

---
+++ Unity Playground/Assets/QuickAccess/TestPlayerScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TestPlayerScript : MonoBehaviour
+{
+    public void Fire(InputAction.CallbackContext context)
+    {
+        Debug.Log("Fire!");
+    }
+}
