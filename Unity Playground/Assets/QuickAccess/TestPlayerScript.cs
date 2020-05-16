f491ec2999952e004d9802b0465beeb8e2f45f13
Manuel-Ko <manuel.kosta@outlook.com>
2020-05-16 19:36:27 +0200

    first experiments with XR input

--- Unity Playground/Assets/QuickAccess/TestPlayerScript.cs
+++ Unity Playground/Assets/QuickAccess/TestPlayerScript.cs
@@ -8,5 +8,25 @@ public class TestPlayerScript : MonoBehaviour
     public void Fire(InputAction.CallbackContext context)
     {
         Debug.Log("Fire!");
+        Debug.Log($"triggererd by {context.control.device.displayName}");
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        var touchInput = context.ReadValue<Vector2>();
+        Debug.Log($"move input vec:{touchInput}");
+        Debug.Log($"triggererd by {context.control.displayName}");
+    }
+
+    public void OpenMenu(InputAction.CallbackContext context)
+    {
+        Debug.Log("open Menu");
+        Debug.Log($"triggererd by {context.control.displayName}");
+    }
+
+    public void GripButton(InputAction.CallbackContext context)
+    {
+        Debug.Log("grip pressed");
+        Debug.Log($"triggererd by {context.control.displayName}");
     }
 }
